ea33093c16c3de0726b626deae1abf818c9fae52
Olivia McKee <olivia.mckee97@gmail.com>
2022-05-09 05:08:35 -0500

    multiple rounds complete

--- assets/js/game.js
+++ assets/js/game.js
@@ -73,7 +73,15 @@ var fight = function(enemyName) {
 
 // run fight function to start game
 for(var i = 0; i < enemyNames.length; i++) {
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
-    fight(enemyNames[i]);
+    if (playerHealth > 0) {
+        window.alert("Welcome to Robot Gladiators! Round " + ( i + 1 ) );
+        var pickedEnemyName = enemyNames[i];
+        enemyHealth = 50;
+        //debugger;
+        fight(pickedEnemyName);
+    }
+    else {
+        window.alert("You have lost your robot in battle! Game Over!");
+        break; 
+    }
 }
